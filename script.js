0f0ee925364f4a665c8550470b136264a197a870
kaungkhant1232282-ship-it <kaungkhant1232282@gmail.com>
2026-04-14 14:28:40 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Monopoly Game Initialized!");
+
+// အနာဂတ်မှာ အန်စာတုံးခေါက်တဲ့ function တွေ ဒီမှာရေးမယ်
+function welcomeMessage() {
+    alert("Monopoly Game ကို ကြိုဆိုပါတယ်!");
+}
+
+// welcomeMessage(); // စမ်းချင်ရင် ရှေ့က // ကို ဖြုတ်လိုက်ပါ
+
